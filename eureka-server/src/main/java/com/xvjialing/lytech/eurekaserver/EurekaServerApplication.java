f20fb60e2278271f4fa3bb2f4757c5e9d929c59d
xvjialing <xvjialing@outlook.com>
2018-02-22 17:02:03 +0800

    完成Eureka服务在DockerCompose环境下的注册与发现

---
+++ eureka-server/src/main/java/com/xvjialing/lytech/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.xvjialing.lytech.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
